61f6f41ae12c5e7c94fbe6bbf1e8e3d83da95055
mcroyaltyhenz <henscheljustice@gmail.com>
2025-07-13 08:30:03 +0100

    Create Description.js

---
+++ Description.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import product from '../product';
+
+const Description = () => {
+  return (
+    <p className="product-description">
+      {product.description}
+    </p>
+  );
+};
+
+export default Description;
